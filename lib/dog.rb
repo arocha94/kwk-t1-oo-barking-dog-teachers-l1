37bd21f78d40dbea3d4bf95e4a8fc7be626f7e14
Alejandra Rocha <arocha94@gmail.com>
2018-06-18 20:16:08 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 # Your code goes here!
 
 class Dog
-  def name
-    .name = name
-  end
+  def .name
+    @.name = .name
+  end 
   
-  def name
-    @name
+  def bark
+    puts "woof!"
   end
 end
